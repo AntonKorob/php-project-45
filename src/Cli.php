e302e24f0cb990b14966f15e2ba95c26f8d671cd
AntonKorob <gorizont102.k@mail.ru>
2025-03-05 17:38:29 +0700

    add .gitignore

--- src/Cli.php
+++ src/Cli.php
@@ -2,10 +2,11 @@
 namespace BrainGames\Cli;
 
 use function WP_CLI\Utils\line;
+use function WP_CLI\Utils\prompt;
 
 function welcome()
 {
     line('Welcome to the Brain Games!');
-    $name = \WP_CLI\Utils\prompt('May I have your name?');
+    $name = prompt('May I have your name?');
     line("Hello, $name!");
 }
