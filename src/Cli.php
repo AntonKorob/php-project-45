e77f93992050f7109356c1a85a65fdde932a6404
AntonKorob <gorizont102.k@mail.ru>
2025-03-08 23:52:21 +0700

    add Engine

--- src/Cli.php
+++ src/Cli.php
@@ -1,11 +1,12 @@
 <?php
 namespace BrainGames\Cli;
 
-use function \wp\cli\line;
+use function cli\line;
+use function cli\prompt;
 
 function welcome()
 {
-    WP_CLI::line('Welcome to the Brain Games!');
-    $name = WP_CLI::prompt('May I have your name?');
-    WP_CLI::line("Hello, $name!");
+    line('Welcome to the Brain Games!');
+    $name = prompt('May I have your name?');
+    line("Hello, $name!");
 }
